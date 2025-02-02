b55e5494ff316e2495291ceebfd1024f68c9fe65
HamHarry <hamza_2641@hotmail.com>
2025-02-02 12:17:38 +0700

    add calendar

--- src/bookingpage/BookingPage.tsx
+++ src/bookingpage/BookingPage.tsx
@@ -1,4 +1,25 @@
 import "./BookingPage.css";
+
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { format } from "date-fns";
+import { parse } from "date-fns/parse";
+import { startOfWeek } from "date-fns/startOfWeek";
+import { getDay } from "date-fns/getDay";
+import { enUS } from "date-fns/locale/en-US";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-datepicker/dist/react-datepicker.css";
+
+const locales = {
+  "en-US": enUS,
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
 const BookingPage = () => {
   return (
     <div className="container-orderpage">
@@ -8,6 +29,17 @@ const BookingPage = () => {
           ตรวจสอบคิว <br />
           ศูนย์พ่นกันสนิม KATS Coating สาขาลาดกระบัง
         </h1>
+      </div>
+      <div className="calendar">
+        <div className="wrap-calendar">
+          <Calendar
+            localizer={localizer}
+            events={[]}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: 520, width: 1250 }}
+          />
+        </div>
       </div>
     </div>
   );
