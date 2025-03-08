e5370ad7e2bb3eeb6240bf3623046e885121ad30
HamHarry <hamza_2641@hotmail.com>
2025-03-08 12:15:03 +0700

    แก้ไขเพิ่มเติมหน้า creactproduct

--- src/addminpage/createproductadminpage/CreateProductAdminPage.tsx
+++ src/addminpage/createproductadminpage/CreateProductAdminPage.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { PRICE_TYPE, ProductDetail } from "../../model/product.type";
 import "./CreateProductAdminPage.css";
+import { CloseCircleFilled } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 const initProductDetail: ProductDetail = {
   type: PRICE_TYPE.STANDARD,
@@ -8,28 +10,90 @@ const initProductDetail: ProductDetail = {
 };
 const CreateProductAdminPage = () => {
   const [productDetails, setProductDetails] = useState<ProductDetail[]>([]);
+  const [openDialogConfirm, setOpenDialogConfirm] = useState<boolean>(false);
+  const navigate = useNavigate();
 
   // เพิ่มข้อมูล
   const addProductDetails = () => {
     setProductDetails([...productDetails, initProductDetail]);
   };
 
+  const rederDialogConfirm = () => {
+    return (
+      <dialog open={openDialogConfirm}>
+        <div className="container-DialogConfirm">
+          <div className="wrap-container-DialogConfirm">
+            <div className="container-DialogConfirm-Navbar">
+              <CloseCircleFilled
+                className="icon-close"
+                onClick={() => {
+                  setOpenDialogConfirm(false);
+                }}
+              />
+            </div>
+            <h1>ยืนยันการจอง</h1>
+            <div className="btn-DialogConfirm-Navbar">
+              <button type="submit" className="btn-submit-dialogConfirm">
+                ยืนยัน
+              </button>
+              <button
+                className="btn-edit-dialogConfirm"
+                onClick={() => {
+                  setOpenDialogConfirm(false); //ส่งข้อมูล
+                }}
+              >
+                ยกเลิก
+              </button>
+            </div>
+          </div>
+        </div>
+      </dialog>
+    );
+  };
+
   return (
     <div className="container-createproductAdmin">
       <div className="header-createproductAdmin">
-        <h1>สร้างสินค้า</h1>
+        <h1>Create Booking</h1>
       </div>
 
-      <div className="content-createproductAdmin">
-        <div className="input-createproduct">
-          <input type="text" placeholder="ชื่อสินค้า" />
-          <input type="text" placeholder="ลักษณะสินค้า" />
-          <input type="text" placeholder="ราคาสินค้า" />
+      <form>
+        <div className="btn-createproductAdmin">
+          <button
+            type="button"
+            onClick={() => {
+              navigate("/admin/product");
+            }}
+          >
+            ย้อนกลับ
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              setOpenDialogConfirm(true);
+            }}
+          >
+            ยืนยัน
+          </button>
         </div>
-        <div className="btn-submit">
-          <button>สร้าง</button>
+        <div className="wrap-container-createproductAdmin">
+          <div className="inputNameProduct">
+            <h2>ชื่อสินค้า</h2>
+            <input type="text" />
+          </div>
+
+          <div className="inputTypeProduct">
+            <h2>ลักษณะสินค้า</h2>
+            <input type="text" />
+          </div>
+
+          <div className="inputPriceProduct">
+            <h2>ราคาสินค้า</h2>
+            <input type="text" />
+          </div>
         </div>
-      </div>
+        {rederDialogConfirm()}
+      </form>
     </div>
   );
 };
